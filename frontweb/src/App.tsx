8b2df2558ff8761cc35682d803f83d755cd89ffb
Geraldo-git <laboratorio.inicial@gmail.com>
2021-11-22 18:35:43 -0300

    Restart project

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,14 +1,25 @@
-import Navbar from 'components/Navbar';
-import './assets/styles/custom.scss';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-  <h1>Hello DSCatalog!</h1>
-  </>
-  
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
